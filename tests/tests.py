267c5bfc5bf68801642cc7d91ce60d34cc912cfb
Eugen Telelyaev <you@example.com>
2024-06-03 22:24:31 +0300

    Edited yaml

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import requests
 import pytest
-from config import BASE_URL
+from config import BASE_URL, TEST_URL
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('__headless__')
+    options.add_argument("--headless=new")
     chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 def test_open_site():
@@ -16,3 +16,6 @@ def test_open_site():
     assert res.status_code == 200
     print(res.status_code)
 
+def test_button_exist(browser):
+    browser.get(TEST_URL)
+    assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
