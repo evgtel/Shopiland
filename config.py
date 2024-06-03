f625496662cef21a2567114a246d8b96b8ed0851
Eugen Telelyaev <you@example.com>
2024-06-03 21:48:14 +0300

    Edited tests

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 BASE_URL = "https://shopiland.ru/"
+TEST_URL = "https://www.qa-practice.com/elements/button/simple"
---
+++ tests/test_button.py
@@ -0,0 +1,16 @@
+import pytest
+from config import BASE_URL, TEST_URL
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture()
+def browser():
+    options = Options()
+    options.add_argument("--headless=new")
+    chrome_browser = webdriver.Chrome(options=options)
+    return chrome_browser
+
+def test_button_exist(browser):
+    browser.get(TEST_URL)
+    assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
