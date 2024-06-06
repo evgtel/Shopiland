1218364e818f1e324952c6a7077a5232acb5f164
Eugen Telelyaev <you@example.com>
2024-06-06 23:22:55 +0300

    Test canonical and reviews begin

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ XPATH_COUNT = "/html/body/div/div/div[2]/div[1]/span"
 XPATH_DOWNLOAD_NEXT = "//button[contains(text(),'Загрузить еще')]"
 BRAND_PAPIA = 'papia'
 BRAND_ZEWA = 'zewa'
+
+XPATH_CANONICAL_LINK = "//link[@rel='canonical']"
+XPATH_COUNT_PRODUCT = "//span[@class='css-meuap9']"
+# //button[@class]
+XPATH_PRODUCTS="//div[@class='css-k9eowz']"
--- tests/tests.py
+++ tests/tests.py
@@ -15,6 +15,7 @@ def browser():
     # options.add_argument("--headless=new")
     chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
+
 def test_open_site():
     res = requests.get(BASE_URL)
     assert res.status_code == 200
@@ -32,17 +33,30 @@ def test_button_exist(browser):
     item = browser.find_elements(By.XPATH, XPATH_CLASS)
 
     p_text = item[0].text
-
-    print(f"Текст элемента = {p_text}, количество = {len(item)}")
+    browser.quit()
+    # print(f"Текст элемента = {p_text}, количество = {len(item)}")
         # try:
         #     next_button = browser.find_element(By.XPATH, XPATH_DOWNLOAD_NEXT)
         #     next_button.click()
         #     browser.implicitly_wait(15)
         # except NoSuchElementException:
         #     button_next_exist = False
-    print(f"Всего товаров: {sum}")
+    # print(f"Всего товаров: {sum}")
+def test_canonical_link_exist(browser):
+    browser.get(BASE_URL+"search?q=стеллаж")
+    count_item = WebDriverWait(browser, 70).until(ec.presence_of_element_located((By.XPATH, XPATH_COUNT)))
+    if count_item:
+        assert len(browser.find_elements(By.XPATH, XPATH_CANONICAL_LINK)) > 0
+    browser.quit()
 
-
-
-        # if el
+def test_reviews_exist(browser):
+    browser.get(BASE_URL + "search?q=стеллаж")
+    count_item = WebDriverWait(browser, 70).until(ec.presence_of_element_located((By.XPATH, XPATH_COUNT)))
+    reviews = browser.find_elements(By.CLASS_NAME, 'reviews')
+    print(count_item.text)
+    print(f"Count rev= {len(reviews)}")
+    assert len(reviews) > 0
+    browser.quit()
+'css-k9eowz'
+def test
     browser.quit()
